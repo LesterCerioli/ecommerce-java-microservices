62387e058a6af828f42499c8ff6f7b4fc17f9cf8
lester <lester.sciensa@qsaude.com.br>
2022-02-23 23:15:15 -0300

    fet(#1): implementing Mail Sender microservice structure

---
+++ microservice-msmailsender/src/main/java/com/ecommerce/msmailsender/MsmailsenderApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.msmailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsmailsenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsmailsenderApplication.class, args);
+	}
+
+}
---
+++ microservice-msmailsender/src/test/java/com/ecommerce/msmailsender/MsmailsenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.msmailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmailsenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
