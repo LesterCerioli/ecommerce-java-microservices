3c472818c4185c99932211386c2519476b92952c
lester <lester.sciensa@qsaude.com.br>
2022-02-24 22:52:20 -0300

    Initial version of msnews generated by generator-jhipster@7.6.0

---
+++ microservice-msnews/src/main/java/com/ecommerce/msnews/config/LoggingAspectConfiguration.java
@@ -0,0 +1,17 @@
+package com.ecommerce.msnews.config;
+
+import com.ecommerce.msnews.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import tech.jhipster.config.JHipsterConstants;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
+    }
+}
